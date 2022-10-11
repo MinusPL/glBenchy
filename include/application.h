ce0540cc0681a69ea0bd5a8c07b4a4afe624027b
Minus <theminusoid@gmail.com>
2022-10-11 18:34:32 +0200

    D2

--- include/application.h
+++
@@ -1,7 +0,0 @@
-#ifndef APPLICATION_H
-#define APPLICATION_H
-
-int glBenchyMain();
-
-
-#endif
--- src/Common/glBenchy.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-#include "../../include/application.h"
-
-int glBenchyMain()
-{
-    printf("Hello from generic main used for all platforms!\n");
-    return 0;
-}
