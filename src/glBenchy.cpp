8a5f421d7b0ffe763524b6c7f132181eb33309bc
Minus <theminusoid@gmail.com>
2022-10-19 16:40:38 +0200

    Drawing something

--- src/glBenchy.cpp
+++ src/glBenchy.cpp
@@ -6,10 +6,21 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
-const char *vertexShaderSource = ""
-    "layout (location = 0) in vec3 aPos;\n"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+#include <cmath>
+
+const char *vertexShaderSource = "#version 300 es\n"
+    "layout (location = 0) in vec3 aPos;\n"         
+    "layout (location = 1) in vec2 inTexCoord;\n"
+    "out vec2 texCoord;\n"
     "void main()\n"
     "{\n"
+    "   texCoord = inTexCoord;\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
@@ -17,7 +28,7 @@ const char *fragmentShaderSource = ""
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(1.0f, 0.8, 0.2f, 1.0f);\n"
     "}\n\0";
 
 
@@ -26,6 +37,9 @@ const char *fragmentShaderSource = ""
 //TODO: Cleanup and atomize parts. Introduce classes/structures.
 int main(int argc, char** argv)
 {
+    char* fragSource;
+    float screen[] = {960.0f, 540.0f};
+    
     //Atomize it to modules etc.
     //This is always called - initialize everything
     glfwInit();
@@ -33,7 +47,7 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 
-    GLFWwindow* winPtr = glfwCreateWindow(1280, 720, "TEST", nullptr, nullptr);
+    GLFWwindow* winPtr = glfwCreateWindow(screen[0], screen[1], "TEST", nullptr, nullptr);
 
     glfwMakeContextCurrent(winPtr);
 
@@ -43,7 +57,7 @@ int main(int argc, char** argv)
     glfwGetFramebufferSize(winPtr, &width, &height);
     glViewport(0, 0, width, height);
 
-    glClearColor(0.2f, 0.6f, 0.9f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     // build and compile our shader program
     // ------------------------------------
@@ -58,18 +72,25 @@ int main(int argc, char** argv)
     if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        //std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+
+    std::ifstream fragmentShaderFile("frag.fs");
+    std::stringstream fragmentStream;
+    fragmentStream << fragmentShaderFile.rdbuf();
+    fragmentShaderFile.close();
+    std::string fragmentCode = fragmentStream.str();
+    const char* fragmentCstr = fragmentCode.c_str();
     // fragment shader
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentCstr, NULL);
     glCompileShader(fragmentShader);
     // check for shader compile errors
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        //std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
     // link shaders
     unsigned int shaderProgram = glCreateProgram();
@@ -80,18 +101,28 @@ int main(int argc, char** argv)
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        //std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-
+    // Format: X, Y, Z, UV.X, UV.Y
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+         1.0f,  1.0f, 0.0f, 1.0f, 1.0f,  // top right
+         1.0f, -1.0f, 0.0f, 1.0f, 0.0f // bottom right
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f // bottom left
+        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f // top left 
     };
+    float quadVerts[] = {
+        -1.0, -1.0, 0.0,      0.0, 0.0,
+        -1.0, 1.0,  0.0,      0.0, 1.0,
+        1.0, -1.0,  0.0,      1.0, 0.0,
+
+        1.0, -1.0, 0.0,       1.0, 0.0,
+        -1.0, 1.0, 0.0,       0.0, 1.0,
+        1.0, 1.0,  0.0,       1.0, 1.0
+    };
+
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
@@ -104,13 +135,16 @@ int main(int argc, char** argv)
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVerts), quadVerts, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
@@ -124,6 +158,8 @@ int main(int argc, char** argv)
     glBindVertexArray(VAO);
 
     glUseProgram(shaderProgram);
+    glUniform2fv(glGetUniformLocation(shaderProgram, "iResolution"), 1, &screen[0]);
+
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -133,9 +169,16 @@ int main(int argc, char** argv)
     ImGui_ImplGlfw_InitForOpenGL(winPtr, true);
     ImGui_ImplOpenGL3_Init("#version 300 es");
 
+    float deltaTime = 0.0f; 
+    float lastFrame = 0.0f; 
+
     //Loop!
     while (!glfwWindowShouldClose(winPtr))
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame; 
+        
         //begin ImGui Frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
@@ -145,11 +188,14 @@ int main(int argc, char** argv)
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-         // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glUseProgram(shaderProgram);
+        glUniform1f(glGetUniformLocation(shaderProgram, "iTime"), fmod(currentFrame, 60.f)); 
+        
+        // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        //ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(winPtr);
         glfwPollEvents();  
